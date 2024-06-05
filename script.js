19e78d7f99feaac0ee3992aa8aceade12765a259
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-06-05 21:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
+
+document.getElementById("closeModalBtn").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+});
